d442483423edb5af636f53f629d74ab319316257
Zeph Lafassett <zeph@lafassett.com>
2014-10-14 10:26:37 -0700

    adding commandline arguments to generating data

--- generate-data.js
+++ generate-data.js
@@ -1,8 +1,27 @@
 var fs = require('fs'),
-	path = require('path');
-	var data = [];
+	args = require('minimist')(process.argv.slice(2)),
+	path = require('path'),
+	size = args.size || 1000000,
+	filename = args.filename || 'test.json',
+	data = [];
 
-for (var i = 0; i < 1000000; i++) {
+if (args.help) {
+	console.log("command line options");
+	console.log("--size: \t number of documents to generate.");
+	console.log("\t\t defaults to `1000000`");
+	console.log("");
+	console.log("--filename: \t the file name of the JSON document that is generated. defaults to `test.json`");
+	console.log("\t\t defaults to `test.json``");
+	console.log("");
+	console.log("example:");
+	console.log("node generate-data.js --size 100 --filename foobar.json");
+	console.log("");
+
+	process.exit();
+}
+
+
+for (var i = 0; i < size; i++) {
 	var item = {
 		id: i,
 		name: "name_" + Math.random(36),
@@ -11,6 +30,6 @@ for (var i = 0; i < 1000000; i++) {
 	data.push(item);
 }
 data = JSON.stringify(data, null, 4);
-fs.writeFile(path.join(__dirname, 'test.json'), data, function (err) {
+fs.writeFile(path.join(__dirname, filename), data, function (err) {
 	if (err) throw err;
 });
